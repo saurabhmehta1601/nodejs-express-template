5d1ce7db6131fbb27b0d5394be81094a7ae2cf7c
saurabh <saurabhmehta1601@gmail.com>
2021-08-20 13:53:41 +0530

    add login route

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -32,9 +32,59 @@ export const register = async (
       httpOnly: true,
       maxAge: 60 * 60 * 24 * 30 * 4,
     });
-    return res.status(201).json({ username, accessToken });
+    return res.status(201).json({ data: { username, accessToken } });
   } catch (err) {
     err.statusCode = 500;
+    return next(err);
+  }
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      const err: ICustomError = new Error("Credentials missing");
+      err.statusCode = 400;
+      return next(err);
+    }
+
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      const err: ICustomError = new Error("Please check credentials");
+      err.statusCode = 400;
+      return next(err);
+    }
+
+    user.comparePassword(password, async (err, isMatch: boolean) => {
+      if (err) {
+        return next(err);
+      } else {
+        if (isMatch) {
+          const refreshToken = getRefreshToken({ username });
+          const accessToken = getAccessToken({ username });
+
+          user.refreshToken = refreshToken
+          await user.save()
+
+          res.cookie("refresh-token", refreshToken, {
+            secure: true,
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 30 * 4,
+          });
+          return res.status(200).json({ data: { accessToken } });
+        } else {
+          const err: ICustomError = new Error("Please check credentials");
+          err.statusCode = 400;
+          return next(err);
+        }
+      }
+    });
+  } catch (err) {
     return next(err);
   }
 };
